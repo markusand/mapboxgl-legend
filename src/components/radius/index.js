markusand/mapboxgl-legend

commit 73144250e9aa24539e67b44306df146fa25e63d9

refactor: Remove unnecessary checks

src/components/radius/index.js
@@ -2,11 +2,10 @@ import { createElement, serializeLabel } from '/@/utils';
 import './bubbles.scss';
 
 export default (expression, { metadata }) => {
-  if (!expression) return null;
-  return createElement('ul', {
+  const { stops } = expression;
+  createElement('ul', {
     classes: 'bubbles',
-    content: expression.stops
-      .sort((a, b) => b[1] - a[1]) // order from bigger to smaller
+    content: stops.sort((a, b) => b[1] - a[1]) // order from bigger to smaller
       .map(([value, radius]) => createElement('li', {
         styles: { '--radius': `${radius}px` },
         content: createElement('span', {

src/components/color/color-list/index.js
@@ -2,10 +2,10 @@ import { createElement, serializeLabel } from '/@/utils';
 import './list.scss';
 
 export default (expression, { metadata }) => {
-  if (!expression) return null;
+  const { stops } = expression;
   return createElement('ul', {
     classes: ['list', 'list--color'],
-    content: expression.stops.map(([value, color]) => createElement('li', {
+    content: stops.map(([value, color]) => createElement('li', {
       styles: { '--color': color },
       content: serializeLabel(value, metadata),
     })),

src/components/color/gradient-bar/index.js
@@ -2,7 +2,6 @@ import { createElement, serializeLabel, map } from '/@/utils';
 import './gradient.scss';
 
 export default (expression, { metadata }) => {
-  if (!expression) return null;
   const { inputs, stops, min, max } = expression;
   const gradient = stops.map(([value, color]) => `${color} ${map(value, min, max)}%`);
   return createElement('div', {

src/components/color/index.js
@@ -2,7 +2,6 @@ import GradientBar from './gradient-bar';
 import BulletList from './color-list';
 
 export default (expression, layer) => {
-  if (!expression) return null;
   switch (expression.name) {
     case 'interpolate':
       return GradientBar(expression, layer);

src/components/image/index.js
@@ -1,10 +1,10 @@
 import { createElement, serializeLabel } from '/@/utils';
 
 export default (expression, { metadata }, map) => {
-  if (!expression) return null;
+  const { stops } = expression;
   return createElement('ul', {
     classes: ['list', 'list--icons'],
-    content: expression.stops.map(([value, image]) => {
+    content: stops.map(([value, image]) => {
       const { height, width, data } = map.style.imageManager.images[image]?.data || {};
       if (!height || !width || !data) return null;
       const size = Math.max(width, height);

src/index.js
@@ -57,8 +57,7 @@ export default class LegendControl {
             ...Object.entries({ ...layout, ...paint }).map(([attr, expression]) => {
               const [, property] = attr.split('-');
               const parsed = Expression.parse(expression);
-              const component = components[property];
-              return parsed && component && component(parsed, layer, this._map);
+              return parsed && components[property]?.(parsed, layer, this._map);
             }),
           ],
         });

commit b4dcb7999c04e1f0f9ae14018a074924016ef836

fix: Error introduced in 73144250e9aa24539e67b44306df146fa25e63d9

src/components/radius/index.js
@@ -3,7 +3,7 @@ import './bubbles.scss';
 
 export default (expression, { metadata }) => {
   const { stops } = expression;
-  createElement('ul', {
+  return createElement('ul', {
     classes: 'bubbles',
     content: stops.sort((a, b) => b[1] - a[1]) // order from bigger to smaller
       .map(([value, radius]) => createElement('li', {