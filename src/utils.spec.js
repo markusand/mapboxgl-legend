17c0176afb7d9e3463e6b2910ff76014df943990
Marc Vilella <mvilella.and@gmail.com>
2022-05-18 15:56:15 +0200

    test: Utils unit tests

---
+++ src/utils.spec.js
@@ -0,0 +1,102 @@
+/* eslint-disable import/no-unresolved */
+import { describe, it, expect } from 'vitest';
+import { ensureArray, map, chunk, zip, toPair, toBins, createElement, serializeLabel } from './utils';
+
+describe('Expression utilities', () => {
+  it('should encapsulate variables in array if needed', () => {
+    expect(ensureArray('string')).toEqual(['string']);
+    expect(ensureArray(['string'])).toEqual(['string']);
+  });
+
+  it('should rescale a value to new bounds', () => {
+    expect(map(1, 0, 4, 0, 100)).toBe(25);
+  });
+
+  it('should create chunks of arrays from array', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(chunk(array, 2)).toEqual([[1, 2], [3, 4], [5, 6]]);
+    expect(chunk(array, 3)).toEqual([[1, 2, 3], [4, 5, 6]]);
+    expect(chunk(array, 4)).toEqual([[1, 2, 3, 4], [5, 6]]);
+  });
+
+  it('should zip multiple arrays', () => {
+    const arrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    expect(zip(...arrays)).toEqual([[1, 4, 7], [2, 5, 8], [3, 6, 9]]);
+  });
+
+  it('should return always an array of 2 elements', () => {
+    expect(toPair([1, 2])).toEqual([1, 2]);
+    expect(toPair([1])).toEqual([null, 1]);
+  });
+
+  it('should create bins of range values', () => {
+    const stops = [[0, 'a'], [1, 'b'], [2, 'c'], [3, 'd'], [4, 'e'], [null, 'f']];
+    expect(toBins(stops)).toEqual([
+      [[0, 1], 'a'],
+      [[1, 2], 'b'],
+      [[2, 3], 'c'],
+      [[3, 4], 'd'],
+      [[4, null], 'e'],
+      [null, 'f'],
+    ]);
+  });
+});
+
+describe('DOM utilities', () => {
+  it('should create an element', () => {
+    const element = createElement('div', {
+      classes: ['class1', 'class2'],
+      styles: { color: 'red' },
+      attributes: { id: 'test' },
+      content: 'Hello',
+    });
+    expect(element.classList.contains('class1')).toBe(true);
+    expect(element.classList.contains('class2')).toBe(true);
+    expect(element.style.color).toBe('red');
+    expect(element.id).toBe('test');
+    expect(element.textContent).toBe('Hello');
+  });
+
+  it('should create an element with children elements', () => {
+    const element = createElement('div', {
+      content: [
+        createElement('span', { content: 'Hello' }),
+        createElement('span', { content: 'World' }),
+      ],
+    });
+    expect(element.childElementCount).toBe(2);
+    expect(element.firstElementChild.tagName).toBe('SPAN');
+    expect(element.firstElementChild.textContent).toBe('Hello');
+    expect(element.lastElementChild.tagName).toBe('SPAN');
+    expect(element.lastElementChild.textContent).toBe('World');
+  });
+
+  it('should append created elementselements', () => {
+    const parent = createElement('div');
+    const child = createElement('div', {
+      appendTo: parent,
+    });
+    expect(parent.childElementCount).toBe(1);
+    expect(parent.firstElementChild).toBe(child);
+  });
+});
+
+describe('Labels', () => {
+  it('should serialize a simple number label', () => {
+    expect(serializeLabel(1)).toBe('1');
+    expect(serializeLabel(1, { unit: 'm' })).toBe('1m');
+    expect(serializeLabel(1, { labels: { 1: 'one' } })).toBe('one');
+  });
+
+  it('should serialize a range label', () => {
+    expect(serializeLabel([1, 2])).toBe('1 - 2');
+    expect(serializeLabel([1, 2], { labels: { 1: 'one', 2: 'two' } })).toBe('one - two');
+    expect(serializeLabel([1, null])).toBe('+1');
+  });
+
+  it('should serialize the OTHER label', () => {
+    expect(serializeLabel(null)).toBe('other');
+    expect(serializeLabel(null, { unit: 'm' })).toBe('other');
+    expect(serializeLabel(null, { labels: { other: 'Altres' } })).toBe('Altres');
+  });
+});
