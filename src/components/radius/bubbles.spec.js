001bfba9b626026ead6aba1155b68a90383c4a39
Marc Vilella <mvilella.and@gmail.com>
2022-05-19 13:43:13 +0200

    test: Add components unit tests

---
+++ src/components/radius/bubbles.spec.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-unresolved */
+import { describe, it, expect } from 'vitest';
+import Bubbles from './index';
+
+describe('Radius panel', () => {
+  it('should create a panel with bubbles', () => {
+    const expression = { stops: [[1, 10], [2, 20], [3, 30]] };
+    const metadata = { labels: { 1: 'one', 3: 'three' } };
+    const el = Bubbles(expression, { metadata });
+
+    expect(el.tagName).toBe('UL');
+    expect(el.classList.contains('bubbles')).toBeTruthy();
+    expect(el.childElementCount).toBe(3);
+
+    const { firstElementChild: first, lastElementChild: last } = el;
+    expect(first.tagName).toBe('LI');
+    expect(first.getAttribute('style')).toBe('--radius: 30px;');
+    expect(first.firstElementChild.tagName).toBe('SPAN');
+    expect(first.firstElementChild.textContent).toBe('three');
+    expect(last.getAttribute('style')).toBe('--radius: 10px;');
+  });
+});
