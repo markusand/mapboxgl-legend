99d4121ceaa459380210a803c2739c661fd2b2e8
Marc Vilella <mvilella.and@gmail.com>
2023-08-02 14:52:29 +0200

    style: Add spacing for legibility

--- src/expression.ts
+++ src/expression.ts
@@ -19,13 +19,22 @@ const parse = (input: any): ParsedExpression<any, any>[] => {
   const [name, ...args] = isExpression(input)
     ? input
     : ['literal' as ExpressionName, input];
+
   if (name === 'case') return args.slice(1).flatMap(parse);
+
   const stops = stopper[name]?.(args);
-  const getter = name === 'literal' ? undefined : ['match', 'step'].includes(name) ? args[0] : args[1];
   if (!stops) return [];
+  
+  const getter = name === 'literal'
+    ? undefined
+    : ['match', 'step'].includes(name)
+      ? args[0]
+      : args[1];
+
   const [inputs, outputs] = zip(...stops);
   const min = Math.min(...inputs.flat(2) as number[]);
   const max = Math.max(...inputs.flat(2) as number[]);
+
   return [{ name, getter, stops, inputs, outputs, min, max }];
 };
 
